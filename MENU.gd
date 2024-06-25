c4607536e78acc17f4b4c539dacf0968a1a9ac18
Clara Machado <claramachado2005@gmail.com>
2024-06-24 23:54:35 -0300

    Tentatia de Menu, não deu certo

---
+++ MENU.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_iniciar_pressed():
+	pass # Replace with function body.
+
+
+func _on_opções_pressed():
+	get_tree().change_scene_to_file("res://MENU_CONTROLE.tscn")
+
+
+func _on_sair_pressed():
+	get_tree().quit()
