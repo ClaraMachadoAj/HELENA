7e8337421b474925ed660d0b2642446d65bed18b
Clara Machado <claramachado2005@gmail.com>
2024-06-25 00:01:06 -0300

    menu funcionando

--- MENU.gd
+++ MENU.gd
@@ -1,13 +1,10 @@
 extends Control
 
-
 func _on_iniciar_pressed():
 	pass # Replace with function body.
 
-
 func _on_opções_pressed():
 	get_tree().change_scene_to_file("res://MENU_CONTROLE.tscn")
-
 
 func _on_sair_pressed():
 	get_tree().quit()
