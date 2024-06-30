4761ef085d578f574f3decd445c24f88bb4d6e0a
Anderson Gomes <agrmv@cesar.school>
2024-06-29 23:54:09 -0300

    Update diagonally movimentation

--- JOGO/PLAYER.gd
+++ JOGO/PLAYER.gd
@@ -12,16 +12,17 @@ func _physics_process(delta):
 	
 	# Movimentação horizontal
 	var direction_x = Input.get_axis("LEFT", "RIGHT")
-	if direction_x:
+	if direction_x != 0:
 		velocity.x = direction_x * SPEED
+		velocity.y = 0  # Zerar a velocidade vertical se movendo horizontalmente
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	# Movimentação vertical
-	var direction_y = Input.get_axis("UP", "DOWN")
-	if direction_y:
-		velocity.y = direction_y * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+		# Movimentação vertical
+		var direction_y = Input.get_axis("UP", "DOWN")
+		if direction_y != 0:
+			velocity.y = direction_y * SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
