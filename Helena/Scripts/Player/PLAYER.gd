05880bc80fc05f9e21c2cd27236d173ad0d59129
FelipeNunes <felipe.nunes.morgado@gmail.com>
2024-07-02 12:15:58 -0300

    Animação
    Adicionado configurações de animação

--- Helena/Scripts/Player/PLAYER.gd
+++ Helena/Scripts/Player/PLAYER.gd
@@ -2,10 +2,14 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const SPRINT_MULTIPLIER = 1.5
+var last_direction = 0.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var anim = get_node("AnimatedSprite2D")
+	
+	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -21,15 +25,31 @@ func _physics_process(delta):
 	var direction_x = Input.get_axis("LEFT", "RIGHT")
 	if direction_x != 0:
 		velocity.x = direction_x * current_speed
-		velocity.y = 0  # Zerar a velocidade vertical se movendo horizontalmente
+		velocity.y = 0 
+		if direction_x > 0:
+			anim.play("Right")
+			last_direction = 1
+		else:
+			anim.play("Left")
+			last_direction = 2
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if last_direction == 2:
+			anim.play("Idle_Left")
+		if last_direction == 1:
+			anim.play("Idle_Right")
 		
 		# Movimentação vertical
 		var direction_y = Input.get_axis("UP", "DOWN")
 		if direction_y != 0:
 			velocity.y = direction_y * current_speed
+			if direction_y > 0:
+				anim.play("Down")
+				last_direction = 1
+			else:
+				anim.play("Up")
 		else:
 			velocity.y = move_toward(velocity.y, 0, SPEED)
+			anim.play("Idle_down")
 
 	move_and_slide()
